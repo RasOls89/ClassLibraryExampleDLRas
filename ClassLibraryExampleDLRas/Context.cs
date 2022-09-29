955dd236e3c15884ff311eb74554e0f0775b36ba
Rasmus Olsson <rasols8911@gmail.com>
2022-09-29 08:35:49 +0200

    Added UOW and Rep bases

---
+++ ClassLibraryExampleDLRas/Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ClassLibraryExampleDLRas
+{
+    public class Context : DbContext
+    {
+        public Context(string connectionString) : base(GetOptions(connectionString))
+        {
+            
+        }
+
+
+        private static DbContextOptions GetOptions(string connectionString)
+        {
+            return SqlServerDbContextOptionsExtensions.UseSqlServer(new DbContextOptionsBuilder(), connectionString).Options;
+        }
+
+    }
+}
