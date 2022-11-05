254433063f12ff8e611113327ca809c9e3eaa961
Rasmus Olsson <S192312@student.hb.se>
2022-11-05 12:23:28 +0100

    Working on it

--- ClassLibraryExampleDLRas/Internals/Context.cs
+++ ClassLibraryExampleDLRas/Internals/Context.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
+using Models;
 
 namespace ClassLibraryExampleDLRas
 {
---
+++ GUI/Views/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BusinessLayer.Controllers;
+using BusinessLayer;
+using Models;
+
+namespace GUI.Views
+{
+    class MainViewModel
+    {
+    }
+}
